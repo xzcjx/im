d31f38fed182242b6de08d144c50d12a1c71b18d
junxi <junxi@mindrank.ai>
2024-01-15 17:53:06 +0800

    开发netty-server

---
+++ chat_netty_server/src/main/java/com/xzccc/server/exception/InvalidFrameException.java
@@ -0,0 +1,8 @@
+package com.xzccc.server.exception;
+
+public class InvalidFrameException extends Exception {
+
+    public InvalidFrameException(String s) {
+        super(s);
+    }
+}
--- chat_netty_server/src/main/java/com/xzccc/server/server/ChatServer.java
+++ chat_netty_server/src/main/java/com/xzccc/server/server/ChatServer.java
@@ -24,15 +24,27 @@ public class ChatServer {
     public void run() {
         ServerBootstrap bg = new ServerBootstrap();
 
-        try{
+        try {
             boss = new NioEventLoopGroup(1);
             work = new NioEventLoopGroup();
 
-            bg.group(boss, work).channel(NioServerSocketChannel.class).localAddress(new InetSocketAddress(port)).option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
-                    .childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
-                        //有连接到达时会创建一个channel
-                        protected void initChannel(SocketChannel ch) throws Exception {
+            // 设置reactor模式的boss和woker线程
+            bg.group(boss, work);
+
+            // 设置Nio类型的Channel
+            bg.channel(NioServerSocketChannel.class);
+
+            // 绑定监听端口
+            bg.localAddress(new InetSocketAddress(port));
+
+            // 设置通道选项
+            bg.option(ChannelOption.SO_KEEPALIVE, true);
+            bg.option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
+            bg.childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
+
+            bg.childHandler(new ChannelInitializer<SocketChannel>() {
+                //有连接到达时会创建一个channel
+                protected void initChannel(SocketChannel ch) throws Exception {
 //                // 管理pipeline中的Handler
 //                ch.pipeline().addLast(new SimpleProtobufDecoder());
 //                ch.pipeline().addLast(new SimpleProtobufEncoder());
@@ -41,8 +53,8 @@ public class ChatServer {
 //                ch.pipeline().addLast("login",loginRequestHandler);
 ////                    ch.pipeline().addLast(chatRedirectHandler);
 //                ch.pipeline().addLast(serverExceptionHandler);
-                        }
-                    });
+                }
+            });
             ChannelFuture channelFuture = bg.bind().sync();
             channelFuture.addListener(new ChannelFutureListener() {
                 @Override
@@ -52,10 +64,9 @@ public class ChatServer {
             });
             // 7 监听通道关闭事件
             // 应用程序会一直等待，直到channel关闭
-            ChannelFuture closeFuture =
-                    channelFuture.channel().closeFuture();
+            ChannelFuture closeFuture = channelFuture.channel().closeFuture();
             closeFuture.sync();
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
             // 8 优雅关闭EventLoopGroup，
