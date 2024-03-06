649c2ee09d94527eba29c1785c6c1f6c6716b8cb
junxi <junxi@mindrank.ai>
2024-03-06 14:54:00 +0800

    关闭liquibase执行sql

--- chat-server/src/main/java/com/xzccc/server/config/LiquibaseConfig.java
+++ chat-server/src/main/java/com/xzccc/server/config/LiquibaseConfig.java
@@ -15,7 +15,7 @@ public class LiquibaseConfig {
         //指定changelog的位置，这里使用的一个master文件引用其他文件的方式
         liquibase.setChangeLog("classpath:liquibase/master.yml");
         //liquibase.setContexts("development,test,production");
-        liquibase.setShouldRun(true);
+        liquibase.setShouldRun(false);
         return liquibase;
     }
 }
